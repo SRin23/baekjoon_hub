8bf2fdfdc9d25c82ef9e08dc4e9f30743a748567
김세린 <68180798+SRin23@users.noreply.github.com>
2022-03-08 20:42:11 +0900

    [Bronze IV] Title: 연세대학교, Time: 200 ms, Memory: 17544 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte n = sc.nextByte();
+        if(n==0){
+            System.out.println("YONSEI");
+        }else{
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+}
