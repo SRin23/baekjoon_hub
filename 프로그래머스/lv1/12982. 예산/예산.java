2fcaf302f26ad3cf7a4fda79d124c2e9e9653745
김세린 <68180798+SRin23@users.noreply.github.com>
2022-03-09 16:40:17 +0900

    [level 1] Title: 예산, Time: 0.18 ms, Memory: 68.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        for(int i = 0; i<d.length-1; i++){
+            for(int j = i+1; j<d.length; j++){
+                if(d[i]>d[j]){
+                    int tmp = d[i];
+                    d[i] = d[j];
+                    d[j] = tmp;
+                }   
+            }
+        }
+        
+        /*
+        for(int i = 0; i<d.length; i++){
+            System.out.println(d[i]);
+        }
+        */
+        
+        for(int i = 0; i<d.length; i++){
+            if(budget - d[i]>=0){
+                budget -= d[i];
+                answer++;
+            }
+            
+        }
+        return answer;
+    }
+}
