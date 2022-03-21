c7e266c62efb48e6fe3c024cd3350b615b3c5ceb
김세린 <68180798+SRin23@users.noreply.github.com>
2022-03-21 09:31:29 +0900

    [Silver V] Title: 소트인사이드, Time: 208 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] values = Long.toString(sc.nextLong()).split("");
+        Arrays.sort(values, Collections.reverseOrder());
+        System.out.println(Long.parseLong(String.join("", values)));;
+    }
+}
