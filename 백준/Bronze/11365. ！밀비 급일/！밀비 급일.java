523a15eb26dd6620afbba2e25770ffbe34d18cfc
김세린 <68180798+SRin23@users.noreply.github.com>
2022-03-23 19:39:13 +0900

    [Bronze II] Title: !밀비 급일, Time: 128 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+            String str = br.readLine();
+            if(str.equals("END")) break;
+            StringBuffer sb = new StringBuffer(str);
+
+            System.out.println(sb.reverse().toString());
+        }
+    }
+}
