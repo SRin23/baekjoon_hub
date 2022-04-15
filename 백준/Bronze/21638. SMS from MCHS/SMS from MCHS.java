254a04056e9ef2d55fff011a9367c46cc2befde5
김세린 <68180798+SRin23@users.noreply.github.com>
2022-04-15 12:51:21 +0900

    [Bronze IV] Title: SMS from MCHS, Time: 212 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/21638. SMS from MCHS/SMS from MCHS.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t1 = sc.nextInt();
+        int v1 = sc.nextInt();
+        int t2 = sc.nextInt();
+        int v2 = sc.nextInt();
+
+        if(t2<0&&v2>=10) System.out.println("A storm warning for tomorrow! Be careful and stay home if possible!");
+        else if(t2<t1) System.out.println("MCHS warns! Low temperature is expected tomorrow.");
+        else if(v2>v1) System.out.println("MCHS warns! Strong wind is expected tomorrow.");
+        else System.out.println("No message");
+
+    }
+}
