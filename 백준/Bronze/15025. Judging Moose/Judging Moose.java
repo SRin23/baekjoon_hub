7de81e6ca4534e16f31da6bfe2ce029e393a9e29
김세린 <68180798+SRin23@users.noreply.github.com>
2022-04-17 16:41:11 +0900

    [Bronze IV] Title: Judging Moose, Time: 236 ms, Memory: 18296 KB -BaekjoonHub

---
+++ 백준/Bronze/15025. Judging Moose/Judging Moose.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+
+        if(l==r&&r==0){
+            System.out.println("Not a moose");
+        }
+        else if(l==r){
+            System.out.println("Even " + (l+r));
+        }else{
+            System.out.println("Odd " + Math.max(l, r)*2);
+        }
+    }
+}
